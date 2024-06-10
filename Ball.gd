4ecca14e33eb8c06ebca40c3827f538ece0d321a
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-09 20:21:06 -0700

    Refactor Ball and play sound during collisions

--- Ball.gd
+++ Ball.gd
@@ -2,23 +2,24 @@ extends CharacterBody2D
 
 var speed = 400
 var ball_velocity = Vector2.ZERO
+@onready var ball_sound = $BallSound
 
 func _ready():
-	ball_velocity.x = [1,-1][randi()%2]
-	ball_velocity.y = [-0.8, 0.8][randi()%2]
+	start_ball()
 
 func _physics_process(delta):
 	var collision_object = move_and_collide(ball_velocity*speed * delta)
 	if collision_object:
 		ball_velocity = ball_velocity.bounce(collision_object.get_normal())
+		$BallSound.play()
 
 func reset_ball():
 	position = Vector2(640,200)
 
 func stop_ball():
 	speed = 0
-	
-func restart_ball():
+
+func start_ball():
 	speed = 400
 	ball_velocity.x = [1,-1][randi()%2]
 	ball_velocity.y = [-0.8, 0.8][randi()%2]
