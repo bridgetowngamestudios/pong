ebe70638b4a445fe005749a464f687e66b200e8e
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-09 20:22:07 -0700

    Use Ball's new functions and start creation of pause menu

--- World.gd
+++ World.gd
@@ -6,23 +6,37 @@ var Player2Score = 0
 func _on_Left_body_entered(body):
 	if body.name != "Ball":
 		return
+	$Ball.stop_ball()
 	$Ball.reset_ball()
 	Player2Score +=1
-	get_tree().call_group('BallGroup', 'stop_ball')
 	$Countdown.start()
 
 func _on_Right_body_entered(body):
 	if body.name != "Ball":
 		return
+	$Ball.stop_ball()
 	$Ball.reset_ball()
 	Player1Score += 1
-	get_tree().call_group('BallGroup', 'stop_ball')
 	$Countdown.start()
 
 func _process(delta):
 	$Player1Score.text = str(Player1Score)
 	$Player2Score.text = str(Player2Score)
-
+	if Player1Score == 10:
+		pass 
+		# Player win condition
+	elif Player2Score == 10:
+		pass
+		# AI win condition
 
 func _on_Countdown_timeout():
-	get_tree().call_group('BallGroup', 'restart_ball')
+	$Ball.start_ball()
+
+func _on_restart_button_pressed():
+	pass # Replace with function body.
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
