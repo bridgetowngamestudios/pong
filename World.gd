8b3e83f60edb50d1ecdba22d91a2881a87e0521b
Taylor Fahlman <fahlmantaylor@gmail.com>
2024-06-11 09:48:08 -0700

    Play win or lose sounds when conditions are met

--- World.gd
+++ World.gd
@@ -16,7 +16,7 @@ func pause_game():
 
 func unpause_game():
 	get_tree().paused = false
-	$PauseMenu.hide
+	$PauseMenu.hide()
 
 func _on_Left_body_entered(body):
 	if body.name != "Ball":
@@ -24,6 +24,7 @@ func _on_Left_body_entered(body):
 	$Ball.stop_ball()
 	$Ball.reset_ball()
 	Player2Score +=1
+	$PointSound.play()
 	$Countdown.start()
 
 func _on_Right_body_entered(body):
@@ -32,27 +33,42 @@ func _on_Right_body_entered(body):
 	$Ball.stop_ball()
 	$Ball.reset_ball()
 	Player1Score += 1
+	$PointSound.play()
 	$Countdown.start()
 
 func _process(delta):
 	$Player1Score.text = str(Player1Score)
 	$Player2Score.text = str(Player2Score)
-	if Player1Score == 10:
-		pass 
-		# Player win condition
-	elif Player2Score == 10:
-		pass
-		# AI win condition
+	if !get_tree().paused:
+		if Player1Score == 10:
+			pause_game()
+			$PlayerWinSound.play()
+			show_message("Player Won!")
+		elif Player2Score == 10:
+			pause_game()
+			$PlayerLoseSound.play()
+			show_message("Enemy Won...")
+
+func show_message(message):
+	$WonLabel.text = message
+	$WonLabel.show()
+
+func hide_message():
+	$WonLabel.text = ""
+	$WonLabel.hide()
 
 func _on_Countdown_timeout():
 	$Ball.start_ball()
 
 func _on_restart_button_pressed():
+	pause_game()
+	hide_message()
 	Player1Score = 0
 	Player2Score = 0
 	$Ball.stop_ball()
 	$Ball.reset_ball()
 	$Countdown.start()
+	unpause_game()
 
 func _on_quit_button_pressed():
 	get_tree().quit()
